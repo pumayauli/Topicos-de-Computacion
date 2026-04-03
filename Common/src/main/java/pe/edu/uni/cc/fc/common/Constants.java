d10dcf81abb3a40cf52a2f4fe67ccce48740a754
pumayauli <bryan.pumayauli.e@uni.pe>
2026-04-02 20:43:03 -0500

    Addapting generic CaesarCipher class into a modular class

---
+++ Common/src/main/java/pe/edu/uni/cc/fc/common/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.uni.cc.fc.common;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Constants {
+    public static final int ALPHABET_SET_SIZE = 26;
+    
+}
