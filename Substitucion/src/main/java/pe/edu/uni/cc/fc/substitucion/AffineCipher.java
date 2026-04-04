2bc84f28683f68ac6b009dc062f09ff65741563f
pumayauli <bryan.pumayauli.e@uni.pe>
2026-04-04 11:28:35 -0500

    Add implementation of cipher Functions

--- Substitucion/src/main/java/pe/edu/uni/cc/fc/substitucion/AffineCipher.java
+++ Substitucion/src/main/java/pe/edu/uni/cc/fc/substitucion/AffineCipher.java
@@ -4,6 +4,8 @@
  */
 package pe.edu.uni.cc.fc.substitucion;
 
+import static pe.edu.uni.cc.fc.common.Constants.ALPHABET_SET_SIZE;
+
 /**
  *
  * @author Usuario
@@ -12,11 +14,35 @@ public class AffineCipher {
     
     public static String encrypt(String plainText,int a,int b){
         String result = "";
+        for(int i=0;i<plainText.length();i++){
+            char c = plainText.charAt(i);
+            if(Character.isLetter(c)){
+               System.out.println(c);   
+               char offset = Character.isUpperCase(c)? 'A' : 'a';
+               // x= c -offset
+               c = (char)(((a*(c-offset)+b) % ALPHABET_SET_SIZE) + offset);
+            }     
+            result +=c;
+        }
         return result;
         
     }
     public static String decrypt(String encryptText,int a ,int b){
         String result = "";
+        //calculo del inverso modular
+        int inv = 19;
+        //
+        for (int i=0;i<encryptText.length();i++){
+            char c =encryptText.charAt(i);
+            if(Character.isLetter(c)){
+                char offset=Character.isUpperCase(c) ? 'A' : 'a';
+                // x = c -offset
+                int tmp = inv*(c-offset-b + ALPHABET_SET_SIZE);
+                c = (char) (tmp % ALPHABET_SET_SIZE + offset);
+                
+            }
+            result +=c;
+        }
         return result;
     }
     
